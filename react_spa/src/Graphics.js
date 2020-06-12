817f07e65280292810a4e24e3fc96143b1cca5d7
RC-croft <zaccro5625@students.ecpi.edu>
2020-06-12 13:55:05 -0400

    final commit

--- react_spa/src/Graphics.js
+++ react_spa/src/Graphics.js
@@ -5,10 +5,45 @@ class Graphics extends Component {
     return (
       <div >
         <ul class="options">
-          <h1>Resolution <button class="res">Fullscreen</button><button class="res">Windowless</button><button class="res">Windowed</button>
+          <h1>Resolution   <button class="res">Fullscreen</button><button class="res">Windowless</button><button class="res">Windowed</button>
           </h1>
         </ul>
-        <ul></ul>
+      <ul class="options">
+        <h1>Draw Distance <input
+    type="range"
+    min="10"                    // default 0
+    max="1000"                  // default 100
+    step="10"                   // default 1
+    value="700"                 // default min + (max-min)/2
+    data-orientation="vertical" // default horizontal
+/></h1></ul>
+<ul class="options">
+        <h1>Camera FOV <input
+    type="range"
+    min="10"                    // default 0
+    max="1000"                  // default 100
+    step="10"                   // default 1
+    value="100"                 // default min + (max-min)/2
+    data-orientation="vertical" // default horizontal
+/></h1></ul>
+<ul class="options">
+        <h1>Brightness <input
+    type="range"
+    min="10"                    // default 0
+    max="1000"                  // default 100
+    step="10"                   // default 1
+    value="600"                 // default min + (max-min)/2
+    data-orientation="vertical" // default horizontal
+/></h1></ul>
+<ul class="options">
+        <h1>Graphics Quality <input
+    type="range"
+    min="10"                    // default 0
+    max="1000"                  // default 100
+    step="10"                   // default 1
+    value="500"                 // default min + (max-min)/2
+    data-orientation="vertical" // default horizontal
+/></h1></ul>
       </div>
     );
   }
